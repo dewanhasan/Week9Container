7d86e6574631be734803febe44328ba63638033a
DEWAN HASAN - STUDENT <g00410156@atu.ie>
2023-11-22 15:16:24 +0000

    First Commit

---
+++ src/main/java/com/example/week9container/Week9containerApplication.java
@@ -0,0 +1,13 @@
+package com.example.week9container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week9containerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week9containerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/week9container/Week9containerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week9container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week9containerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
